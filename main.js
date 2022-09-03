714ab2028be0580d16a9ca5245545f08e51413ac
islammd39e <islammdrahat308@gmail.comm>
2022-09-03 22:24:58 +0600

    upload data

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-URL('https://openapi.programming-hero.com/api/news/categories')
+// URL('https://openapi.programming-hero.com/api/news/categories')
+// https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a
+// https://openapi.programming-hero.com/api/news/category/01
+
+const loadCetegory = async()=>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+   console.log(data.data.news_category);
+}
+
+const displayNews = news =>{
+    
+
+}
+
+
+loadCetegory()
