17757b1d92babfa45adbcdc49053332eef9b87c6
islammd39e <islammdrahat308@gmail.comm>
2022-09-18 15:08:20 +0600

    upload via file

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const loadCart = async(category_id)=>{
-    const url = `https://openapi.programming-hero.com/api/news/category/0${category_id}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    loadAllNews(data.data)
-    
+const loadCart = async (category_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/0${category_id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  loadAllNews(data.data)
+
 }
-const loadAllNews = alls =>{
-    const allUpdateNews = document.getElementById('all-news');
-    allUpdateNews.innerHTML='';
-    alls.forEach(allNew => {
-       const div = document.createElement('div')
-       div.classList.add('col')
-       div.innerHTML=`
-       <div class="card">
-       <img src="${allNew.image_url}" class="card-img-top" alt="...">
-       <div class="card-body">
-         <h5 class="card-title">${allNew.title}</h5>
-         <p class="card-text">${allNew.details}</p>
-         <img src="${allNew.author.img}" class="card-img" alt="...">
+const loadAllNews = alls => {
+  const allUpdateNews = document.getElementById('all-news');
+  allUpdateNews.innerHTML = '';
+  alls.forEach(allNew => {
+    const div = document.createElement('div')
+    // console.log(allNew.details);
+    let details = allNew.details;
+    // console.log(details);
+    const sortText = details.slice(0, 200) + "..."
+    div.classList.add('col')
+    div.innerHTML = `
+      <div class="card">
+      <img src="${allNew.image_url}" class="card-img-top" alt="...">
+      <div class="card-body">
+      <h5 class="card-title">${allNew.title}</h5>
+      <p class="card-text">${sortText}</p>
+      <img src="${allNew.author.img}" class="card-img" alt="...">
          <p class="text">${allNew.author.name}</p>
          <p class="publish-text">${allNew.author.published_date}</p>
          <p class="view-text">total view ${allNew.total_view}</p>
@@ -27,12 +31,12 @@ const loadAllNews = alls =>{
      </div>
      
        `;
-      allUpdateNews.appendChild(div)
-   });
-    
+    allUpdateNews.appendChild(div)
+  });
+
 }
 
- loadCart()
+loadCart()
 
 
 
--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // https://openapi.programming-hero.com/api/news/0282e0e58a5c404fbd15261f11c2ab6a
 // https://openapi.programming-hero.com/api/news/category/01
 
-const loadCetegory = async()=>{
+const loadCetegory = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data = await res.json();
@@ -10,9 +10,9 @@ const loadCetegory = async()=>{
 
 }
 
-const displayNews = news =>{
+const displayNews = news => {
     const userId = document.getElementById('user-id')
-    news.forEach(createNews =>{
+    news.forEach(createNews => {
         const newsDiv = document.createElement('ul');
         newsDiv.innerHTML = `
         <l1 class="nav-item"><a href="#" class='text-de' onclick ="loadCart(${createNews.category_id})">${createNews.category_name}</a></l1>
